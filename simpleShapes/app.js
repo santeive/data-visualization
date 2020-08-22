d344a87a14ba33b1000aa36d8db7eecd58ea9b13
santeive <packo.ive@gmail.com>
2020-08-22 16:09:23 -0500

    Some shapes for D3

---
+++ simpleShapes/app.js
@@ -0,0 +1,7 @@
+const canvas = d3.select(".canva");
+
+const svg = canvas.append("svg")
+    .attr('width', "500")
+    .attr("height", "500");
+
+svg.append("circle").attr("cx", "100").attr("cy", "100").attr("r", "50").attr("fill", "blue")
