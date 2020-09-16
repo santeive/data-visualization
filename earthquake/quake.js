167442415aed766806765ccea64754cc23a7aa1a
santeive <packo.ive@gmail.com>
2020-09-15 20:55:49 -0500

    Event mouseover

--- earthquake/quake.js
+++ earthquake/quake.js
@@ -2,7 +2,7 @@ const canvas = d3.select(".canva");
 
 let width = "100%";
 let height = "100%";
-const api_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
+const api_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
 
 const svg = canvas.append("svg")
     .attr('width', width)
@@ -25,5 +25,19 @@ d3.json(api_url)
             .attr("cx", (d,i) => Math.floor(Math.random()*200) + d.properties.mag*i)
             .attr("cy", (d,i) => Math.floor(Math.random()*100) + d.properties.mag)
             .attr("r", (d,i) => (d.properties.mag)*2)
+            .style('top', 156)
+            .on("mouseover", function(d, i, n){
+                d3.select(n[i])
+                .transition()
+                .duration(100)// in miliseconds
+                .style("opacity", 0.7)
+                console.log(d.properties.mag)
+            })
+            .on("mouseout", function(d, i, n){
+                d3.select(n[i])
+                .transition()
+                .duration(100)// in miliseconds
+                .style("opacity", 1)
+            })
             .attr("fill", (d,i) => d.properties.alert);
 });
